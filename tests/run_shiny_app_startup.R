87e18774d0894120da05849780576095e2a9c2e5
Jiawang1209 <Jiawang1209@163.com>
2026-05-25 23:45:59 +0800

    fix: make startup smoke reusable

--- tests/run_shiny_app_startup.R
+++ tests/run_shiny_app_startup.R
@@ -1,4 +1,11 @@
-repo_root <- normalizePath(file.path("..", ".."), mustWork = TRUE)
+args <- commandArgs(trailingOnly = FALSE)
+file_arg <- grep("^--file=", args, value = TRUE)
+script_path <- if (length(file_arg)) {
+  normalizePath(sub("^--file=", "", file_arg[[1]]), mustWork = TRUE)
+} else {
+  normalizePath(file.path("tests", "run_shiny_app_startup.R"), mustWork = TRUE)
+}
+repo_root <- normalizePath(file.path(dirname(script_path), ".."), mustWork = TRUE)
 setwd(file.path(repo_root, "inst", "app"))
 
 source("global.R")
