609551d2fb66825ce047bc90b50197cb63c74a8a
Jiawang1209 <Jiawang1209@163.com>
2026-05-25 18:39:36 +0800

    feat: wire Data Hub uploads to registry

--- inst/app/modules/mod_data_hub.R
+++ inst/app/modules/mod_data_hub.R
@@ -16,6 +16,41 @@ mod_data_hub_ui <- function(id) {
 
 mod_data_hub_server <- function(id, registry) {
   shiny::moduleServer(id, function(input, output, session) {
+    shiny::observeEvent(input$register, {
+      shiny::req(input$file)
+
+      result <- tryCatch(
+        {
+          table <- read_user_table(input$file$datapath, filename = input$file$name)
+          type <- detect_upload_type(table)
+          validation <- if (identical(type, "matrix")) validate_matrix_like(table) else app_success(table)
+
+          if (!validation$ok) {
+            shiny::showNotification(validation$message, type = "error")
+            return(NULL)
+          }
+
+          item <- registry_add(
+            registry,
+            name = input$object_name,
+            type = type,
+            data = validation$value,
+            source = input$file$name,
+            warnings = validation$warnings
+          )
+
+          shiny::showNotification(paste("Registered", item$name), type = "message")
+          item
+        },
+        error = function(e) {
+          shiny::showNotification(conditionMessage(e), type = "error")
+          NULL
+        }
+      )
+
+      invisible(result)
+    })
+
     output$objects <- DT::renderDT(registry_list(registry), rownames = FALSE)
   })
 }
